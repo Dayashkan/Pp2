fd20ff9b37cb61713554b8441f7a11811458c5ff
Dayashkan <dayashkan.99@gmail.com>
2019-02-08 18:39:12 +0600

    dsfsadf

---
+++ week1/task1/task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reading a line and converting it to integer
+            int n = int.Parse(Console.ReadLine());
+            //reading the numbers and splitting them by space
+            string[] numb = Console.ReadLine().Split(' ');
+            //creating a new list
+            List<int> list = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                //converting the numbers to integers
+                int x = int.Parse(numb[i]);
+                int ok = 1;
+                // Identifying the primes
+                for (int j = 2; j <= x / 2; j++)
+                {
+                    if (x % j == 0)
+                    {
+                        ok = 0;
+                        break;
+                    }
+                }
+                // adding primes to the list
+                if (ok == 1 && x > 1)
+                    list.Add(x);
+            }
+            //displaying the number of elements in the list w primes
+            Console.WriteLine(list.Count);
+            for (int i = 0; i < list.Count; i++)
+                //displaying the primes
+                Console.Write(list[i] + " ");
+            Console.ReadKey();
+        }
+    }
+}
