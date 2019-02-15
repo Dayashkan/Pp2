cb992c62a2237fd43ef55ddb80b6d4d7d8abad19
Dayashkan <dayashkan.99@gmail.com>
2019-02-15 16:08:59 +0600

    asdf

--- week2/task4/task4/Program.cs
+++ week2/task4/task4/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace task4
 {
@@ -10,16 +11,14 @@ namespace task4
     {
         static void Main(string[] args)
         {
-            //writing somethimg to our text file
-            StreamWriter sw = new StreamWriter(@"D:\Users\Ceasar\Desktop\Pp2\week2\task4\path\ex.txt");
-            sw.Write("hi");
-            sw.Close();
+           
             string path = (@"D:\Users\Ceasar\Desktop\Pp2\week2\task4\path\ex.txt");
             string path1 = (@"D:\Users\Ceasar\Desktop\Pp2\week2\task4\path1\ex.txt");
+            FileStream fs = File.Create(path);
+            fs.Close();
+            
             //copying the file
-            File.Copy(path, path1);
-            File.Delete(path);
-            sw.Close();
+            File.Copy(path, path1, true);
             Console.ReadKey();
 
         }
